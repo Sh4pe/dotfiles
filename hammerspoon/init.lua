96b6da84db56710752f3f60cd98f02d592550981
David Nies <nies.david@googlemail.com>
2015-06-18 14:00:13 +0200

    Add tab movement for Safari and Terminal

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,5 +1,33 @@
 -- Reloading the config
+--
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
   hs.reload()
   hs.alert.show("Config loaded")
+end)
+
+
+-- next tab movement
+--
+hs.hotkey.bind({"cmd"}, "]", function()
+  local activeWin = hs.window.focusedWindow()
+  local activeApp = activeWin:application()
+
+  if activeApp:title() == "Safari" then
+    activeApp:selectMenuItem({"Fenster", "Nächsten Tab anzeigen"})
+  elseif activeApp:title() == "Terminal" then
+    activeApp:selectMenuItem({"Fenster", "Nächsten Tab anzeigen"})
+  end
+end)
+
+-- previous tab movement
+--
+hs.hotkey.bind({"cmd"}, "[", function()
+  local activeWin = hs.window.focusedWindow()
+  local activeApp = activeWin:application()
+
+  if activeApp:title() == "Safari" then
+    activeApp:selectMenuItem({"Fenster", "Vorherigen Tab anzeigen"})
+  elseif activeApp:title() == "Terminal" then
+    activeApp:selectMenuItem({"Fenster", "Vorherigen Tab anzeigen"})
+  end
 end)
