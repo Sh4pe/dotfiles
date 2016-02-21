a52a776c25e20923048b4a85025e6e220803d6f9
David Nies <nies.david@googlemail.com>
2016-02-21 17:36:17 +0100

    Install tpm in tmux' install script

--- setup-scripts/tmux/setup.sh
+++ setup-scripts/tmux/setup.sh
@@ -2,7 +2,15 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-## Make symlink if it's not there
+## Install tpm if necessary
+TPM_DIR=$HOME/.tmux/plugins/tpm
+if [[ ! -d "$TPM_DIR" ]]; then
+    echo "Cloning tpm..."
+    mkdir -p $TPM_DIR
+    git clone https://github.com/tmux-plugins/tpm $TPM_DIR
+fi
+
+## Make symlink to .tmux.conf if it's not there
 if [[ ! -h "$HOME/.tmux.conf" ]]; then
     echo "Creating symlink for .tmux.conf..."
     ln -s $(realpath $SCRIPT_DIR/../../tmux.conf) $HOME/.tmux.conf
